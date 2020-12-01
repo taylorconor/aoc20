de60b791b0051ad79339fad36329ec1b1a309bf2
Conor Taylor <conor@taylor.sh>
2020-12-01 09:28:57 +0100

    Day 1

---
+++ 1/1.py
@@ -0,0 +1,8 @@
+with open('1/input.txt') as f:
+    inputs = [int(x) for x in f]
+
+for input in inputs:
+    diff = 2020 - input
+    if diff in inputs:
+        print(str(input * diff))
+        exit()
---
+++ 1/2.py
@@ -0,0 +1,10 @@
+with open('1/input.txt') as f:
+    inputs = [int(x) for x in f]
+
+for first_input in inputs:
+    first_diff = 2020 - first_input
+    for second_input in inputs:
+        second_diff = first_diff - second_input
+        if second_diff in inputs:
+            print(str(first_input * second_input * second_diff)))
+            exit()
