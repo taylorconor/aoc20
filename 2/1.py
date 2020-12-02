3caa432d0fd91618ddd38dd49cc9a4c93bd3650c
Conor Taylor <conor@taylor.sh>
2020-12-02 09:40:48 +0100

    Day 2

---
+++ 2/1.py
@@ -0,0 +1,16 @@
+import re
+
+regex = r"([0-9]+)-([0-9]+)\ ([a-z]): ([a-z]*)"
+with open('2/input.txt') as f:
+    matches = re.finditer(regex, f.read(), re.MULTILINE)
+
+valid_passwords = 0
+for match in matches:
+    count = 0
+    for char in match.group(4):
+        if char == match.group(3):
+            count = count + 1
+    if count >= int(match.group(1)) and count <= int(match.group(2)):
+        valid_passwords = valid_passwords + 1
+
+print(str(valid_passwords))
---
+++ 2/2.py
@@ -0,0 +1,21 @@
+import re
+
+regex = r"([0-9]+)-([0-9]+)\ ([a-z]): ([a-z]*)"
+with open('2/input.txt') as f:
+    matches = re.finditer(regex, f.read(), re.MULTILINE)
+
+valid_passwords = 0
+for match in matches:
+    count = 0
+    lower = int(match.group(1)) - 1
+    upper = int(match.group(2)) - 1
+    password = match.group(4)
+
+    if len(password) > lower and password[lower] == match.group(3):
+        count = count + 1
+    if len(password) > upper and password[upper] == match.group(3):
+        count = count + 1
+    if count == 1:
+        valid_passwords = valid_passwords + 1
+
+print(str(valid_passwords))
