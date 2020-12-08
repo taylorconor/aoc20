0477c16218d83f1f018647b72ef1f240477eac30
Conor Taylor <conor@taylor.sh>
2020-12-08 09:58:39 +0100

    Day 8

---
+++ 8/1.py
@@ -0,0 +1,21 @@
+import re
+
+with open('8/input.txt') as f:
+    instructions = [[x, False] for x in f]
+
+regex = r"([a-z]+) ([\+-][0-9]+)"
+pc = 0
+acc = 0
+while pc < len(instructions):
+    if instructions[pc][1]:
+        break
+    instructions[pc][1] = True
+    instruction = re.match(regex, instructions[pc][0])
+    if instruction[1] == "jmp":
+        pc += int(instruction[2])
+        continue
+    elif instruction[1] == "acc":
+        acc += int(instruction[2])
+    pc += 1
+
+print(str(acc))
---
+++ 8/2.py
@@ -0,0 +1,35 @@
+import re
+
+with open('8/input.txt') as f:
+    data = f.read().splitlines()
+
+regex = r"([a-z]+) ([\+-][0-9]+)"
+
+
+def terminating_acc(instructions):
+    pc = 0
+    acc = 0
+    while pc < len(instructions):
+        if instructions[pc][1]:
+            return 0
+        instructions[pc][1] = True
+        instruction = re.match(regex, instructions[pc][0])
+        if instruction[1] == "jmp":
+            pc += int(instruction[2])
+            continue
+        elif instruction[1] == "acc":
+            acc += int(instruction[2])
+        pc += 1
+    return acc
+
+
+for i in range(len(data)):
+    ins = [[x, False] for x in data]
+    if ins[i][0][:3] == "jmp":
+        ins[i][0] = ins[i][0].replace("jmp", "nop")
+    elif ins[i][0][:3] == "nop":
+        ins[i][0] = ins[i][0].replace("nop", "jmp")
+    acc = terminating_acc(ins)
+    if acc > 0:
+        print(str(acc))
+        break
